bc9327331e95f61ce46f5c7285a55a3872b0bcdc
hafsa <hfsyln93@gmail.com>
2022-10-15 14:19:00 +0300

    fınısh

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ import { data } from "../helpers/data";
 
 const Card = () => {
   return (
-    <Container className="rounded-4 mt-4 p-4" style={{background: "f48b29"}}>
+    <Container className="rounded-4 mt-4 p-4" style={{background: "#f48b29"}}>
         <h1 className="text-white my-2">Language</h1>
         <Row className="g-3 justify-content-center">
             {data.map((lang, index) => {
--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -2,30 +2,33 @@ import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 
-const Language = ({name, img, options}) => {
-    const [showDesc, setShowDesc] = useState(false)
-    console.log(name, img, options);
-  return (
-    
-    <Container 
-        onClick={() => setShowDesc(!showDesc)} 
-        style={{ background: "peachpuff"}} 
-        className="p-4 rounded-2">
-            
-            {!showDesc && (
-      <Container>
-        <Image src={img} width="70%"></Image>
-        <h3 className="display-6">{name}</h3>
-      </Container>  
-            )}
+const Language = ({ name, img, options }) => {
+  const [showImage, setShowImage] = useState(true);
 
-            {showDesc && (
-      <ul>
-        {options.map((e) => {
-            return <li className="h5 text-start">{e}</li>
-            })}
-      </ul>
-            )}
+  // console.log(name, img, options);
+  console.log(showImage);
+
+  return (
+    <Container
+      style={{ background: "peachpuff" }}
+      className="p-4 rounded-2 h-100 lang-card"
+      onClick={() => setShowImage(!showImage)}
+      type="button"
+    >
+      {showImage && (
+        <Container>
+          <Image className="lang-logo" src={img} width="70%"></Image>
+          <h3 className="display-6">{name}</h3>
+        </Container>
+      )}
+
+      {!showImage && (
+        <ol className="h-100 d-flex flex-column justify-content-center">
+          {options.map((item) => {
+            return <li className="h5 text-start">{item}</li>;
+          })}
+        </ol>
+      )}
     </Container>
   );
 };
